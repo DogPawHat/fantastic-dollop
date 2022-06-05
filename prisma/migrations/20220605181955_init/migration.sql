d0d4bae0a24dd7e355aaea72b095bf4257f94370
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2022-06-05 20:49:19 +0100

    fix: database schema

--- prisma/migrations/20220605181955_init/migration.sql
+++ prisma/migrations/20220605181955_init/migration.sql
@@ -1,9 +1,12 @@
 -- CreateTable
 CREATE TABLE "Comment" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" TEXT NOT NULL PRIMARY KEY,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
     "authorName" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "upvotes" INTEGER NOT NULL DEFAULT 0
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Comment_id_key" ON "Comment"("id");
